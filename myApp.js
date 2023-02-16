1b829fa97cd76f49264202ec4c885e5da54569f7
bhk31 <bhkhandelwal92@gmail.com>
2023-02-17 00:05:52 +0530

    createAndSavePerson

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 // let Person;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const Schema = mongoose.Schema;
 const personSchema = new Schema({
   name: { type: String, required: true },
@@ -14,7 +15,12 @@ const personSchema = new Schema({
 const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+  
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
